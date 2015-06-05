7afb57076be5dd74dfbffc15e894fff6f0e90690
Michele Ancheta <michelee.ancheta@gmail.com>
2015-06-05 08:45:41 -0430

    cambiando en inicio

--- app/view/Main.js
+++ app/view/Main.js
@@ -1,50 +1,21 @@
-Ext.define('Sencha.view.Main', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'main',
-    requires: [
-        'Ext.TitleBar',
-        'Ext.Video'
+Ext.define('Itunes.view.Main', {
+    extend: 'Ext.Container',
+    xtype : 'main',
+    requires : [
+        
     ],
-    config: {
-        tabBarPosition: 'bottom',
-
-        items: [
-            {
-                title: 'Welcome',
-                iconCls: 'home',
-
-                styleHtmlContent: true,
-                scrollable: true,
-
-                items: {
-                    docked: 'top',
-                    xtype: 'titlebar',
-                    title: 'Welcome to Sencha Touch 2'
-                },
-
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
-            },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
-
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
-            }
-        ]
+    config : {
+        layout  : 'vbox',                  //Step 1
+        items   : [{
+            xtype   : 'container',
+            height  : 120,                 //Step 2
+            style   : 'background:#bdc3c7',
+            html    : 'The Header'
+        },{
+            xtype   : 'component',
+            flex    : 1,                   //Step 3
+            style   : 'background:#ecf0f1',
+            html    : 'Te Results'
+        }]
     }
 });
